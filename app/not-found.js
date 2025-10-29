e8bd28ca6753674afb64b077a57d0780b57f1633
Naman Gond <gondnaman739@gmail.com>
2025-10-29 11:20:35 +0530

    add critters and not-found

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "80px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
